748a619ce16260744eccc6f39eb6f31228f13eb2
Admin <kopylov.a98@mail.ru>
2023-12-20 22:16:56 +0500

    commit

--- booking/urls.py
+++ booking/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for booking project.
+URL configuration for booking1 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
